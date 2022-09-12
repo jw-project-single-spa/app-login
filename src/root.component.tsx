e6d4b4d0f9470042357a0f72d32e9161626c8bca
Eduardo Dallmann <eduardodallmann@gmail.com>
2022-09-12 15:25:54 -0300

    login

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,22 @@
-export default function Root(props) {
-  return <section>{props.name} is mounted! App login aqui</section>;
+export default function Root(props: {
+  name: string;
+  firebaseLibApp: typeof import("firebase/app");
+  firebaseLibAuth: typeof import("firebase/auth");
+  firebaseLibFirestore: typeof import("firebase/firestore");
+}) {
+  const {
+    firebaseLibApp: { getApp },
+    firebaseLibAuth: { signInWithRedirect, getAuth, GoogleAuthProvider },
+  } = props;
+
+  const redirect = () => {
+    signInWithRedirect(getAuth(getApp()), new GoogleAuthProvider());
+  };
+
+  return (
+    <section>
+      {props.name} is mounted! App login aquis
+      <button onClick={redirect}>logar</button>
+    </section>
+  );
 }
